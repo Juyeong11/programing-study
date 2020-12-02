07a8c476768d006ebfece1e3a2e45a5288c70bf0
qazqwerqaz <43169966+qazqwerqaz@users.noreply.github.com>
2020-12-02 21:06:30 +0900

    이동연산자

---
+++ C++ for expert/Spreadsheet.cpp
@@ -0,0 +1,91 @@
+#include<stdexcept>
+#include "Spreadsheet.h"
+
+Spreadsheet::Spreadsheet(size_t width, size_t height):mWidth(width),mHeight(height)
+{
+	mCells = new SpreadsheetCell * [mWidth];
+	for (size_t i = 0; i < mWidth; ++i) {
+		mCells[i] = new SpreadsheetCell[mHeight];
+	}
+}
+
+Spreadsheet::~Spreadsheet()
+{
+	for (size_t i = 0; i < mWidth; ++i)
+		delete[] mCells[i];
+	delete[] mCells;
+	mCells = nullptr;
+}
+
+Spreadsheet::Spreadsheet(const Spreadsheet& src):
+	Spreadsheet(src.mWidth,src.mHeight)//위임생성자
+{
+	for (size_t i = 0; i < mWidth; ++i) {
+		for (size_t j = 0; j < mHeight; ++i) {
+			mCells[i][j] = src.mCells[i][j];
+		}
+	}
+}
+
+Spreadsheet& Spreadsheet::operator=(const Spreadsheet& rhs)
+{
+	if (this == &rhs)
+		return *this;
+	//이렇게 하면 중간에 익셉션이 발생해도 객체는 변하지 않는다
+	Spreadsheet temp(rhs);
+	swap(*this, temp);
+	return *this;
+}
+
+Spreadsheet::Spreadsheet(Spreadsheet&& src) noexcept
+	:Spreadsheet()
+{
+	swap(*this, src);
+}
+
+Spreadsheet& Spreadsheet::operator=(Spreadsheet&& rhs) noexcept
+{
+	if (this == &rhs) return *this;
+
+	Spreadsheet temp(std::move(rhs));
+	swap(*this, temp);
+	return *this;
+}
+
+void Spreadsheet::setCellAt(size_t x, size_t y, const SpreadsheetCell& cell)
+{
+	verifyCoordinate(x, y);
+	mCells[x][y] = cell;
+}
+
+SpreadsheetCell& Spreadsheet::getCellAt(size_t x, size_t y)
+{
+	verifyCoordinate(x, y);
+	return mCells[x][y];
+}
+
+void Spreadsheet::cleanup() noexcept
+{
+	for (size_t i = 0; i < mWidth; ++i)
+		delete[] mCells[i];
+
+	delete[] mCells;
+	mCells = nullptr;
+	mWidth = mHeight = 0;
+}
+
+
+
+void Spreadsheet::verifyCoordinate(size_t x, size_t y) const
+{
+	if (x >= mWidth || y > mHeight)
+		throw std::out_of_range("");
+}
+void swap(Spreadsheet& first, Spreadsheet& second)noexcept
+{
+	using std::swap;
+
+	swap(first.mWidth, second.mWidth);
+	swap(first.mHeight, second.mHeight);
+	swap(first.mCells, second.mCells);
+}
---
+++ C++ for expert/Spreadsheet.h
@@ -0,0 +1,28 @@
+#pragma once
+#include<cstddef>
+#include"SpreadsheetCell.h"
+
+class Spreadsheet
+{
+public:
+	Spreadsheet(size_t width, size_t height);
+	//Spreadsheet(const Spreadsheet& src) = delete;
+	~Spreadsheet();
+	Spreadsheet(const Spreadsheet& src);
+	Spreadsheet& operator=(const Spreadsheet& rhs);
+	friend void swap(Spreadsheet& first, Spreadsheet& second)noexcept;
+
+	Spreadsheet(Spreadsheet&& src)noexcept;
+	Spreadsheet& operator=(Spreadsheet&& rhs)noexcept;
+
+	void setCellAt(size_t x, size_t y, const SpreadsheetCell& cell);
+	SpreadsheetCell& getCellAt(size_t x, size_t y);
+private:
+	void cleanup()noexcept;
+	void moveFrom(Spreadsheet& src)noexcept;
+
+	void verifyCoordinate(size_t x, size_t y) const;
+	size_t mWidth = 0;
+	size_t mHeight = 0;
+	SpreadsheetCell** mCells = nullptr;
+};
